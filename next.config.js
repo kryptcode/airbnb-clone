b62bf8610c12acb2e04fb471ca305b32dafd5f80
kryptcode <54333700+kryptcode@users.noreply.github.com>
2022-02-10 21:00:25 +0100

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    images: {
+        domains: ["links.papareact.com"]
+    },
+    env: {
+        mapbox_key: 'pk.eyJ1Ijoia2FtaXNoaWxvaCIsImEiOiJja3k0YXdjMm8wYTV4MnBxbjl3M2wzcHp0In0.Bvty-lJpAp-lOs_0HFOs_g'
+    }
+}
